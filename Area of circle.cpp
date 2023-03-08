796178862ea65aab056dd2879f6709eb4bcb5d17
Tanmoy447 <tanmoyadhikary612@gmail.com>
2023-03-08 18:04:05 +0530

    Initial commit

---
+++ Area of circle.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int radius=33;
+	float pi=3.14;
+	printf("The area of circle is %f",pi*radius*radius);
+	int height=25;
+	printf("Volumn of this cylinder is %f",pi*radius*radius*height);
+	return 0;
+}
